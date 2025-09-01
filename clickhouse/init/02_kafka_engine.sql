ecaa8fbb2ab768a2e2b4f919675757f52f57ef00
Matthew <mslipenchuk267@gmail.com>
2025-09-01 15:53:33 -0400

    Fixed healthcheck, added init job for Kafka. Switched kafka image to regular version (has cli tools)

--- clickhouse/init/02_kafka_engine.sql
+++ clickhouse/init/02_kafka_engine.sql
@@ -45,7 +45,7 @@ CREATE TABLE IF NOT EXISTS ctv.kafka_ctv_events
 )
 ENGINE = Kafka
 SETTINGS
-    kafka_broker_list   = 'kafka:9093',
+    kafka_broker_list   = 'kafka:29092',
     kafka_topic_list    = 'dev.ctv.ad-beacons.raw.v1',
     kafka_group_name    = 'ch-ctv-consumer',
     kafka_format        = 'JSONEachRow',
